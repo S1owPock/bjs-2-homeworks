2350bb9f291cf655db1a0f854c6e87e220502e8b
Max Solodovnikov <maks_solod@list.ru>
2022-12-07 21:27:40 +0300

    Первое задание сделано не до конца

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,5 +1,19 @@
 function getArrayParams(...arr) {
+  let min= arr[0];
+  let max = arr[0];
+  let sum = 0;
+  let avg = (sum/arr.length).toFixed(2);
+  let num = Number(avg);
 
+  for (let i = 0; i < arr.length; i++){
+    if (min > arr[i]) {
+      min = arr[i];
+    }
+    if (max < arr[i]) {
+      max = arr[i];
+    }
+    sum += arr[i];
+  }
   return { min: min, max: max, avg: avg };
 }
 
