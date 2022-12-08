5ced35ae33f2927bd7475fa4e272f67b04fd5d4c
Max Solodovnikov <maks_solod@list.ru>
2022-12-08 15:16:46 +0300

    Все задания выполены

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -18,21 +18,73 @@ function getArrayParams(...arr) {
 }
 
 function summElementsWorker(...arr) {
-
+  let sum = 0;
+  if (arr.length > 0) {
+    for (let i = 0; i < arr.length; i++) {
+      sum += arr[i];
+    }
+  }
+  return sum;
 }
 
 function differenceMaxMinWorker(...arr) {
+  let difference;
 
+  if (arr.length > 0) {
+    difference = Math.max.apply(null, arr) - Math.min.apply(null, arr);
+  }
+  else {
+    difference = 0;
+  }
+  return difference;
 }
 
 function differenceEvenOddWorker(...arr) {
+  let sumEvenElement = 0;
+  let sumOddElement = 0;
 
+  if (arr.length > 0) {
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] % 2 === 0) {
+        sumEvenElement = sumEvenElement + arr[i];
+      }
+      else {
+        sumOddElement = sumOddElement + arr[i];
+      }
+    }
+    return sumEvenElement - sumOddElement;
+  }
+  else {
+    return 0;
+  }
 }
 
 function averageEvenElementsWorker(...arr) {
+  let sumEvenElement = 0;
+  let countEvenElement = 0;
 
+  if (arr.length > 0) {
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] % 2 === 0) {
+        sumEvenElement = sumEvenElement + arr[i];
+        countEvenElement++;
+      }
+    }
+    return sumEvenElement/countEvenElement;
+  }
+  else {
+    return 0;
+  }
 }
 
 function makeWork (arrOfArr, func) {
+  let maxWorkerResult = -Infinity;
 
+  for (let i = 0; i < arrOfArr.length; i++) {
+    const resultFunc = func(...arrOfArr[i]);
+    if (resultFunc > maxWorkerResult) {
+      maxWorkerResult = resultFunc;
+    }
+  }
+  return maxWorkerResult;
 }
