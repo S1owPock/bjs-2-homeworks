877a69c9c8ed39b627b8023934c3c08e760cda4f
Max Solodovnikov <maks_solod@list.ru>
2023-01-21 14:00:16 +0300

    Ошибки исправлены

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -7,7 +7,7 @@ class PrintEditionItem {
         this.type = null;
     }
     fix() {
-        this.state *= 1,5;
+        this.state *= 1.5;
     }
     set state (number) {
         if (number < 0) {
@@ -28,54 +28,34 @@ class PrintEditionItem {
 class Magazine extends PrintEditionItem {
     constructor(name, releaseDate, pagesCount) {
         super(name, releaseDate, pagesCount);
-        this.name = name;
-        this.releaseDate = releaseDate;
-        this.pagesCount = pagesCount;
-        this.state = 100;
         this.type = "magazine";
     }
 }
 class Book extends PrintEditionItem {
     constructor(author, name, releaseDate, pagesCount) {
         super(name, releaseDate, pagesCount, author);
-        this.name = name;
-        this.releaseDate = releaseDate;
-        this.pagesCount = pagesCount;
         this.author = author;
-        this.state = 100;
         this.type = "book";
     }
 }
 class NovelBook extends Book {
     constructor(author, name, releaseDate, pagesCount) {
         super(name, releaseDate, pagesCount);
-        this.name = name;
-        this.releaseDate = releaseDate;
-        this.pagesCount = pagesCount;
         this.author = author;
-        this.state = 100;
         this.type = "novel";
     }
 }
 class FantasticBook extends Book {
     constructor(author, name, releaseDate, pagesCount) {
         super(name, releaseDate, pagesCount);
-        this.name = name;
-        this.releaseDate = releaseDate;
-        this.pagesCount = pagesCount;
         this.author = author;
-        this.state = 100;
         this.type = "fantastic";
     }
 }
 class DetectiveBook extends Book {
     constructor(author, name, releaseDate, pagesCount) {
         super(name, releaseDate, pagesCount);
-        this.name = name;
-        this.releaseDate = releaseDate;
-        this.pagesCount = pagesCount;
         this.author = author;
-        this.state = 100;
         this.type = "detective";
     }
 }
